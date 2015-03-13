e7c540e109f1ec269c971252836cbca0dce90ec5
no-glue <nikolapav1985@gmail.com>
2015-03-13 20:02:53 +0100

    first

---
+++ Main.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
+import           Control.Monad      (forever)
+import qualified Data.Text          as T
+import qualified Network.WebSockets as WS
+
+meow :: WS.Connection -> IO ()
+meow conn = forever $ do
+    msg <- WS.receiveData conn
+    WS.sendTextData conn $ msg `T.append` ", meow"
+
+serverApp :: WS.PendingConnection -> IO ()
+serverApp pendingConn = do
+    conn <- WS.acceptRequest pendingConn
+    meow conn
+
+main :: IO ()
+main = WS.runServer "127.0.0.1" 8080 serverApp
