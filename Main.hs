53387e61aabaf90bb3f19735b4f43831e848781c
no-glue <nikolapav1985@gmail.com>
2015-03-16 19:40:08 +0100

    response

--- Main.hs
+++ Main.hs
@@ -6,18 +6,18 @@ import qualified Network.WebSockets as WS
 connections :: [WS.Connection]
 connections = []
 
-addConnection :: WS.Connection -> [WS.Connection]
-addConnection conn = conn : connections
+addConnection :: WS.Connection -> [WS.Connection] -> [WS.Connection]
+addConnection conn conns = conn : conns
 
-meow :: WS.Connection -> [WS.Connection] -> IO ()
-meow conn conns = forever $ do
+meow :: WS.Connection -> IO ()
+meow conn = forever $ do
     msg <- WS.receiveData conn
-    WS.sendTextData (head conns) $ msg `T.append` ", meow"
+    WS.sendTextData conn $ msg `T.append` ", meow"
 
 serverApp :: WS.PendingConnection -> IO ()
 serverApp pendingConn = do
     conn <- WS.acceptRequest pendingConn
-    meow conn (addConnection conn)
+    meow conn
 
 main :: IO ()
 main = WS.runServer "127.0.0.1" 8080 serverApp
