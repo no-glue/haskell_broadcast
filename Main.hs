3a17f7209ac035fb9aa8c59e41ab7efd3e021200
no-glue <nikolapav1985@gmail.com>
2015-03-16 20:10:28 +0100

    response

--- Main.hs
+++ Main.hs
@@ -1,13 +1,8 @@
 {-# LANGUAGE OverloadedStrings #-}
 import           Control.Monad      (forever)
+import Control.Concurrent
 import qualified Data.Text          as T
 import qualified Network.WebSockets as WS
-
-connections :: [WS.Connection]
-connections = []
-
-addConnection :: WS.Connection -> [WS.Connection] -> [WS.Connection]
-addConnection conn conns = conn : conns
 
 meow :: WS.Connection -> IO ()
 meow conn = forever $ do
