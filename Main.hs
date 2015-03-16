81e64d879c7dff8f8f6409737a324ba077ed386c
no-glue <nikolapav1985@gmail.com>
2015-03-16 02:28:38 +0100

    response

--- Main.hs
+++ Main.hs
@@ -3,15 +3,19 @@ import           Control.Monad      (forever)
 import qualified Data.Text          as T
 import qualified Network.WebSockets as WS
 
-meow :: WS.Connection -> IO ()
+connections = []
+
+addConnection conn conns = conn : conns
+
+meow :: [WS.Connection] -> IO ()
 meow conn = forever $ do
-    msg <- WS.receiveData conn
-    WS.sendTextData conn $ msg `T.append` ", meow"
+    msg <- WS.receiveData (head conn)
+    WS.sendTextData (head conn) $ msg `T.append` ", meow"
 
 serverApp :: WS.PendingConnection -> IO ()
 serverApp pendingConn = do
     conn <- WS.acceptRequest pendingConn
-    meow conn
+    meow (addConnection conn connections)
 
 main :: IO ()
 main = WS.runServer "127.0.0.1" 8080 serverApp
