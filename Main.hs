560a5f59c1c0f863cd84078b53d9a0a16938e21a
no-glue <nikolapav1985@gmail.com>
2015-03-16 02:55:08 +0100

    response

--- Main.hs
+++ Main.hs
@@ -9,15 +9,15 @@ connections = []
 addConnection :: WS.Connection -> [WS.Connection]
 addConnection conn = conn : connections
 
-meow :: [WS.Connection] -> IO ()
-meow conn = forever $ do
-    msg <- WS.receiveData (head conn)
-    WS.sendTextData (head conn) $ msg `T.append` ", meow"
+meow :: WS.Connection -> [WS.Connection] -> IO ()
+meow conn conns = forever $ do
+    msg <- WS.receiveData conn
+    WS.sendTextData (head conns) $ msg `T.append` ", meow"
 
 serverApp :: WS.PendingConnection -> IO ()
 serverApp pendingConn = do
     conn <- WS.acceptRequest pendingConn
-    meow (addConnection conn)
+    meow conn (addConnection conn)
 
 main :: IO ()
 main = WS.runServer "127.0.0.1" 8080 serverApp
